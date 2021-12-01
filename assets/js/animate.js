1789a4c9738565aa95d7e75aa7b588c977170bfa
Ginanjar Pamungkas <ginanjar69919082@gmail.com>
2021-12-02 00:41:22 +0700

    oke

--- assets/js/animate.js
+++ assets/js/animate.js
@@ -1,6 +1,7 @@
 $(document).ready(function() { 
-    $('.header').find('.header-title').addClass('zoomInUp').removeClass('hide')
+    $('.header').find('.header-title').addClass('zoomInFade').removeClass('hide')
     $('.header').find('p').addClass('zoomInFade').removeClass('hide')
+    $('.header').find('.image').addClass('zoomInFade').removeClass('hide')
 })
  
 var controller = new ScrollMagic.Controller();
@@ -23,8 +24,16 @@ new ScrollMagic.Scene({triggerElement: ".victim-count-trigger"})
 new ScrollMagic.Scene({triggerElement: ".victim-story-trigger"})
                                  .triggerHook(0.85)
                                  .on("enter",(e)=>{
-                                    $('.victim-story-effect').removeClass('hide').removeClass('fadeInUp')
-                                    $('.victim-story-effect').addClass('fadeInUp')
+                                    $('.victim-story-effect').find('.heading-title').removeClass('hide').removeClass('fadeInUp')
+                                    $('.victim-story-effect').find('.heading-title').addClass('fadeInUp')
                                  })
-                                //  .addIndicators({name: "1 (duration: 0)"})
+                                 .addTo(controller);
+
+new ScrollMagic.Scene({triggerElement: ".victim-story-effect"})
+                                 .triggerHook(0.70)
+                                 .on("enter",(e)=>{
+                                    $('#container-victim-story, #container-victim, .section-title, .description-title').removeClass('hide').removeClass('fadeInUp')
+                                    $('#container-victim-story, #container-victim, .section-title, .description-title').addClass('fadeInUp')
+                                 })
+                                 // .addIndicators({name: "1 (duration: 0)"})
                                  .addTo(controller);
